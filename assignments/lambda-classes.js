ed424de5d2f619fc673703f4dde798ccc15e8acc
pj-wise <51010291+pj-wise@users.noreply.github.com>
2019-06-13 18:37:13 -0400

    created instructor class

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -10,3 +10,19 @@ class Person{
     return `Hey, my name is ${this.name}, and I am from ${this.location}!`;
   }
 }
+
+class Instructor extends Person{
+    constructor(lambda) {
+      super(lambda)
+  this.specialty = lambda.specialty;
+  this.favLanguage = lambda.favLanguage;
+  this.catchPhrase = lambda.catchPhrase;
+}
+demo(subject){
+  console.log(`Today we are learning about ${subject}`);
+}
+
+grade(student, subject){
+    console.log(`${student.name} recieves a perfect score on ${subject}`);
+}
+}
