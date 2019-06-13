a267c22eefb84f8486423abd7d11fd4c8dc4901a
pj-wise <51010291+pj-wise@users.noreply.github.com>
2019-06-13 19:18:39 -0400

    finished PM and 1 const

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -5,38 +5,68 @@ class Person {
         this.name = data.name;
         this.age = data.age;
         this.location = data.location;
-    }
+    };
     speak() {
         return `Hey, my name is ${this.name}, and I am from ${this.location}!`;
-    }
-}
+    };
+};
 
 class Instructor extends Person {
     constructor(lambda) {
-        super(lambda)
+        super(lambda);
         this.specialty = lambda.specialty;
         this.favLanguage = lambda.favLanguage;
         this.catchPhrase = lambda.catchPhrase;
-    }
+    };
     demo(subject) {
         console.log(`Today we are learning about ${subject}`);
-    }
+    };
 
     grade(student, subject) {
-        console.log(`${student.name} recieves a perfect score on ${subject}`);
-    }
-}
 
-class Student extends Instructor {
+        console.log(`${student.name} recieves a grade of ${} ${subject}`);
+    };
+};
+
+class Student extends Person {
     constructor(lambda) {
-        super(lambda)
+        super(lambda);
         this.previousBackground = lambda.previousBackground;
         this.className = lambda.className;
         this.favSubjects = lambda.favSubjects;
-    }
+    };
     listsSubjects() {
         for(i = 0; i < this.favSubjects.length; i++){
             console.log(this.favSubjects[i]);
         }
-    }
-}
+    };
+    PRAssignment(subject){
+        console.log(`${this.name} has submitted a PR for ${subject}`);
+    };
+    sprintChallenge(subject){
+        console.log(`${this.name} has begun sprint challenge for ${subject}`);
+    };
+};
+
+class ProjectManager extends Instructor {
+    constructor(lambda){
+        super(lambda);
+        this.gradClassName = lambda.gradClassName;
+        this.favInstructor = lambda.favInstructor;
+    };
+    standUp(channel){
+        console.log(`${this.name} announces to ${channel}, @ channel standy times!`);
+    };
+    debugsCode(student, subject){
+        console.log(`${this.name} debugs ${student.name}'s code on ${subject}`);
+    };
+};
+
+const PJ = new Student({
+    name: "Pearl",
+    age: 17,
+    location: 'Florida',
+    previousBackground: 'HTML and CSS',
+    className: 'WEB21',
+    favSubjects: 'food'
+ });
