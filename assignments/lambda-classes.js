bfea775bb66349259d2fc256e7372c43a51d40dd
pj-wise <51010291+pj-wise@users.noreply.github.com>
2019-06-13 18:08:54 -0400

    created Person class

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,12 @@
 // CODE here for your Lambda Classes
+
+class Person{
+    constructor(data) {
+    this.name = data.name;
+    this.age = data.age;
+    this.location = data.location;
+  }
+  speak(){
+    return `Hey, my name is ${this.name}, and I am from ${this.location}!`;
+  }
+}
